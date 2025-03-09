d360cf87bce6750fb895a4f1f13636010da39528
Raimundo <raimundo.delamaza@mayor.cl>
2025-03-09 15:09:24 -0300

    errorBoundary

--- proyecto-react/src/App.jsx
+++ proyecto-react/src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Weather from "./components/weather";
+import ErrorBoundary from "./components/errorBoundary";
 
 const App = () => {
   return (
+    <ErrorBoundary>
     <div className="App">
       <Weather />
     </div>
+    </ErrorBoundary>
   );
 };
 
