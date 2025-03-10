2b598501201df396a93ca14d8ffeefd9c97a0ecb
Raimundo <raimundo.delamaza@mayor.cl>
2025-03-09 23:37:31 -0300

    name change

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
-import Weather from "./components/Weather";
+import Weather from "./components/weather";
 import ErrorBoundary from "./components/ErrorBoundary";
 
 const App = () => {
